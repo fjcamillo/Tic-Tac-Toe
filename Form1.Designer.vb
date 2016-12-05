547f7e37a11c19b9bdd4304733e55a25b907ae2c
Jerhone <fjcamillo.dev@gmail.com>
2016-12-05 13:28:10 +0800

    Elective 3: Visual Basic.NET Seatwork - Tic Tac Toe Game

---
+++ Form1.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.playerOne = New System.Windows.Forms.Label()
+        Me.playerTwo = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(213, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(104, 67)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(323, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(104, 67)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(433, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(104, 67)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.White
+        Me.Button4.Location = New System.Drawing.Point(213, 85)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(104, 67)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.White
+        Me.Button5.Location = New System.Drawing.Point(323, 85)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(104, 67)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.Location = New System.Drawing.Point(433, 85)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(104, 67)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.White
+        Me.Button7.Location = New System.Drawing.Point(213, 158)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(104, 67)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.White
+        Me.Button8.Location = New System.Drawing.Point(323, 158)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(104, 67)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.White
+        Me.Button9.Location = New System.Drawing.Point(433, 158)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(104, 67)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(214, 241)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(323, 73)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Reset"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(565, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Name"
+        '
+        'playerOne
+        '
+        Me.playerOne.AutoSize = True
+        Me.playerOne.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.playerOne.Location = New System.Drawing.Point(12, 25)
+        Me.playerOne.Name = "playerOne"
+        Me.playerOne.Size = New System.Drawing.Size(119, 31)
+        Me.playerOne.TabIndex = 12
+        Me.playerOne.Text = "<name>"
+        '
+        'playerTwo
+        '
+        Me.playerTwo.AutoSize = True
+        Me.playerTwo.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.playerTwo.Location = New System.Drawing.Point(565, 26)
+        Me.playerTwo.Name = "playerTwo"
+        Me.playerTwo.Size = New System.Drawing.Size(119, 31)
+        Me.playerTwo.TabIndex = 13
+        Me.playerTwo.Text = "<name>"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 112)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Win"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(565, 112)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(26, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Win"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(565, 173)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Draw"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(13, 173)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(32, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Draw"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(565, 232)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(30, 13)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "Lose"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(13, 232)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(30, 13)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Lose"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(564, 125)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(29, 31)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "0"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(12, 125)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(29, 31)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "0"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(565, 186)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(29, 31)
+        Me.Label11.TabIndex = 23
+        Me.Label11.Text = "0"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(13, 186)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(29, 31)
+        Me.Label12.TabIndex = 22
+        Me.Label12.Text = "0"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(565, 245)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(29, 31)
+        Me.Label13.TabIndex = 25
+        Me.Label13.Text = "0"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(13, 245)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(29, 31)
+        Me.Label14.TabIndex = 24
+        Me.Label14.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(751, 327)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.playerTwo)
+        Me.Controls.Add(Me.playerOne)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "X"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents playerOne As Label
+    Friend WithEvents playerTwo As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,284 @@
+﻿Public Class Form1
+    Public thingy = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+    Public winColor = Color.Green
+    Public player_One = "V"
+    Public player_Two = "Q"
+
+    Public playerOneWin = 0
+    Public playerTwoWin = 0
+    Public playerOneLose = 0
+    Public playerTwoLose = 0
+    Public playerDraw = 0
+
+    Public drawChecker = 0
+
+    Public Sub resetsfields()
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+        Button1.BackColor = Color.White
+        Button2.BackColor = Color.White
+        Button3.BackColor = Color.White
+        Button4.BackColor = Color.White
+        Button5.BackColor = Color.White
+        Button6.BackColor = Color.White
+        Button7.BackColor = Color.White
+        Button8.BackColor = Color.White
+        Button9.BackColor = Color.White
+        Return
+    End Sub
+
+    Public Sub moveChecker(firstMark, secondMark)
+        If (Form1.ActiveForm.Text = firstMark) Then
+            Form1.ActiveForm.Text = secondMark
+        Else
+            Form1.ActiveForm.Text = firstMark
+        End If
+        Return
+    End Sub
+
+    Public Sub increaseWin(playerInvolved)
+        If (playerInvolved = player_One) Then
+            playerOneWin = playerOneWin + 1
+            Label10.Text = playerOneWin.ToString()
+
+        Else
+            playerTwoWin = playerTwoWin + 1
+            Label9.Text = playerTwoWin.ToString()
+        End If
+        increaseLose(playerInvolved)
+        Return
+    End Sub
+
+    Public Sub increaseLose(playerNotInvolved)
+        If (playerNotInvolved = player_One) Then
+            playerTwoLose = playerTwoLose + 1
+            Label13.Text = playerTwoLose.ToString()
+        Else
+            playerOneLose = playerOneLose + 1
+            Label14.Text = playerOneLose.ToString()
+        End If
+        Return
+    End Sub
+
+    Public Sub increaseDraw()
+        playerDraw = playerDraw + 1
+        Label11.Text = playerDraw.ToString()
+        Label12.Text = playerDraw.ToString()
+        Return
+    End Sub
+
+    Public Sub checkField(checker, colors)
+        If (Button1.Text = checker And Button2.Text = checker And Button3.Text = checker) Then
+            Button1.BackColor = colors
+            Button2.BackColor = colors
+            Button3.BackColor = colors
+            MessageBox.Show("WINNER: " + checker.ToString)
+            resetsfields()
+            increaseWin(checker)
+            Form1.ActiveForm.Text = "X"
+        End If
+        If (Button4.Text = checker And Button5.Text = checker And Button6.Text = checker) Then
+            Button4.BackColor = colors
+            Button5.BackColor = colors
+            Button6.BackColor = colors
+            MessageBox.Show("WINNER: " + checker.ToString)
+            resetsfields()
+            increaseWin(checker)
+            Form1.ActiveForm.Text = "X"
+        End If
+        If (Button7.Text = checker And Button8.Text = checker And Button9.Text = checker) Then
+            Button7.BackColor = colors
+            Button8.BackColor = colors
+            Button9.BackColor = colors
+            MessageBox.Show("WINNER: " + checker.ToString)
+            resetsfields()
+            increaseWin(checker)
+            Form1.ActiveForm.Text = "X"
+        End If
+        If (Button1.Text = checker And Button4.Text = checker And Button7.Text = checker) Then
+            Button1.BackColor = colors
+            Button4.BackColor = colors
+            Button7.BackColor = colors
+            MessageBox.Show("WINNER: " + checker.ToString)
+            resetsfields()
+            increaseWin(checker)
+            Form1.ActiveForm.Text = "X"
+        End If
+        If (Button2.Text = checker And Button5.Text = checker And Button8.Text = checker) Then
+            Button2.BackColor = colors
+            Button5.BackColor = colors
+            Button8.BackColor = colors
+            MessageBox.Show("WINNER: " + checker.ToString)
+            resetsfields()
+            increaseWin(checker)
+            Form1.ActiveForm.Text = "X"
+        End If
+        If (Button3.Text = checker And Button6.Text = checker And Button9.Text = checker) Then
+            Button3.BackColor = colors
+            Button6.BackColor = colors
+            Button9.BackColor = colors
+            MessageBox.Show("WINNER: " + checker.ToString)
+            resetsfields()
+            increaseWin(checker)
+            Form1.ActiveForm.Text = "X"
+        End If
+        If (Button1.Text = checker And Button5.Text = checker And Button9.Text = checker) Then
+            Button1.BackColor = colors
+            Button5.BackColor = colors
+            Button9.BackColor = colors
+            MessageBox.Show("WINNER: " + checker.ToString)
+            resetsfields()
+            increaseWin(checker)
+            Form1.ActiveForm.Text = "X"
+        End If
+        If (Button3.Text = checker And Button5.Text = checker And Button7.Text = checker) Then
+            Button3.BackColor = colors
+            Button5.BackColor = colors
+            Button7.BackColor = colors
+            MessageBox.Show("WINNER: " + checker.ToString)
+            resetsfields()
+            increaseWin(checker)
+            Form1.ActiveForm.Text = "X"
+        End If
+    End Sub
+
+    Public Sub drawGame()
+        If (Button1.Text <> "" And Button2.Text <> "" And Button3.Text <> "" And Button4.Text <> "" And Button5.Text <> "" And Button6.Text <> "" And Button7.Text <> "" And Button8.Text <> "" And Button9.Text <> "") Then
+            MessageBox.Show("No One Wins: It is a draw")
+            Form1.ActiveForm.Text = "X"
+            increaseDraw()
+            resetsfields()
+        End If
+        Return
+    End Sub
+
+
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (Button1.Text = "") Then
+            Button1.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (Button2.Text = "") Then
+            Button2.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (Button3.Text = "") Then
+            Button3.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (Button4.Text = "") Then
+            Button4.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If (Button5.Text = "") Then
+            Button5.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If (Button6.Text = "") Then
+            Button6.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If (Button7.Text = "") Then
+            Button7.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If (Button8.Text = "") Then
+            Button8.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If (Button9.Text = "") Then
+            Button9.Text = Form1.ActiveForm.Text
+            checkField(player_One, winColor)
+            checkField(player_Two, winColor)
+            drawGame()
+            moveChecker(player_One, player_Two)
+        Else
+            MessageBox.Show("You cannot change your move")
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+        Form1.ActiveForm.Text = "X"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Form1.ActiveForm.Text = player_One
+    End Sub
+End Class
+
